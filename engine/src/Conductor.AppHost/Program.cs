784903dfbe13b4b0d8790e31ae33bebe236484e5
james <james_d02@protonmail.com>
2026-02-07 15:24:28 +0000

    js integration

--- engine/src/Conductor.AppHost/Program.cs
+++ engine/src/Conductor.AppHost/Program.cs
@@ -3,7 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var postgres = builder.AddPostgres("postgres");
 var conductorDb = postgres.AddDatabase("conductor");
 
-builder.AddProject<Projects.Conductor_Engine_Api>("conductor-engine-api")
+var engineApi = builder.AddProject<Projects.Conductor_Engine_Api>("conductor-engine-api")
     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
     .WithReference(conductorDb)
     .WaitFor(conductorDb);
@@ -15,11 +15,22 @@ builder.AddProject<Projects.Conductor_Engine_Playground>("conductor-engine-playg
     .WithExplicitStart()
     .WaitFor(conductorDb);
 
-builder.AddProject<Projects.Conductor_Inventory_Api>("conductor-inventory-api")
+var inventoryApi = builder.AddProject<Projects.Conductor_Inventory_Api>("conductor-inventory-api")
     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
     .WithOtlpExporter()
     .WithReference(conductorDb)
     .WaitFor(conductorDb);
+
+builder.AddJavaScriptApp("conductor-client-web", "../../../client/Conductor.Client.Web")
+    .WithPnpm()
+    .WithRunScript("dev")
+    .WithArgs("--port", "3001")
+    .WithBuildScript("build")
+    .WithHttpEndpoint(targetPort: 3001)
+    .WithReference(engineApi)
+    .WithReference(inventoryApi)
+    .WaitFor(engineApi)
+    .WaitFor(inventoryApi);
 
 var app = builder.Build();
 await app.RunAsync();
