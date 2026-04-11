1458f0055c6ec7296eef13e6d816a261f53c1fdc
james <james_d02@protonmail.com>
2026-04-11 23:40:50 +0100

    remove unused constants

--- platform/src/Orchitect.Infrastructure/Inventory/Azure/Constants/AzureCacheConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Orchitect.Infrastructure.Inventory.Azure.Constants;
-
-internal static class AzureCacheConstants
-{
-    internal const string CloudResourceCacheKey = "azure-cloudresources";
-    internal const string CloudSecretCacheKey = "azure-secrets";
-}
